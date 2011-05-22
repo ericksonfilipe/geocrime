60305b201b33764ff80c6111d8a82b98052f2989
Luan <luanbg13@gmail.com>
2011-05-22 16:44:18 -0300

    com o mapa, mas com erro NoClassDefFoundError

---
+++ src/com/ufcg/sig/geocrime/client/Geocrime.java
@@ -0,0 +1,13 @@
+package com.ufcg.sig.geocrime.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Geocrime implements EntryPoint {
+	public void onModuleLoad() {
+		RootPanel.get().add(new PanelPrincipal());
+	}
+}
